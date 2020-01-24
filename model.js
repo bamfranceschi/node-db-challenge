4c0d3a2bf7b9023b44ea0555a88f59e2897e5621
bamfranceschi <anna.franceschi@gmail.com>
2020-01-24 11:12:48 -0600

    created tables, created knex cleanup seed, outlined access methods and endpoints

--- model.js
+++ model.js
@@ -1,1 +1,39 @@
 const db = require("./data/db-config.js");
+
+module.exports = {
+  findProjects,
+  findResources,
+  findById,
+  findTasksByProject,
+  addResource,
+  addProject,
+  addTask
+};
+
+function findProjects() {
+  return;
+}
+
+function findResources() {
+  return;
+}
+
+function findById(id) {
+  return;
+}
+
+function findTasksByProject(id) {
+  return;
+}
+
+function addResource(newRes) {
+  return;
+}
+
+function addProject(newPrj) {
+  return;
+}
+
+function addTask(newTask, id) {
+  return;
+}
